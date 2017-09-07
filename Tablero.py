b165e69896daecfdf98453f479d09596816dcdac
francoda <francodealesandro@gmail.com>
2017-09-06 23:38:38 -0300

    + set_value

--- Tablero.py
+++ Tablero.py
@@ -6,8 +6,8 @@ class Tablero():
         self.tablero = [["-"] * columnas for _ in range(filas)]
 
     def random(self, celulas_vivas):
-        combinaciones = []
         if(celulas_vivas > 0 and celulas_vivas < len(self.tablero)*len(self.tablero)):
+            combinaciones = []
             for fila in range(len(self.tablero)):
                 for columna in range(len(self.tablero)):
                     combinaciones.append((fila,columna))
@@ -18,6 +18,14 @@ class Tablero():
                 celulas_vivas -= 1
         else:
             raise IndexError
+
+    def set_value(self, fila, columna, valor):
+        if fila > 0 and columna > 0 and fila < len(self.tablero[0]) and fila < len(self.tablero):
+            raise IndexError
+        elif valor != "-" or valor != "*":
+            raise Exception("Valor incorrecto(Valores posibles:[-,*])")
+        else:
+            self.tablero[columna][fila] = valor
 
     def imprimir_tablero(self):
         for t in self.tablero:
