55655e07c0eeba50b0040c33d6db25b4f8ec8f19
francoda <francodealesandro@gmail.com>
2017-09-06 21:51:03 -0300

    + Pintar Tabla

--- Menu.py
+++ Menu.py
@@ -3,9 +3,10 @@ from tkinter import *
 class Juego_de_la_Vida(Tk):
     def __init__(self):
         Tk.__init__(self)
-        t = SimpleTable(self, 20,20)
-        t.pack(fill="both", expand ="True")
-        t.set(0,0,"Red")
+        self.wm_title("Juego de la Vida")
+        self.resizable(width=False, height=False)
+        tabla = SimpleTable(self, 20,20)
+        tabla.pack(fill="both", expand ="True")
 
 class SimpleTable(Frame):
     def __init__(self, parent, rows=10, columns=10):
@@ -15,7 +16,8 @@ class SimpleTable(Frame):
         for row in range(rows):
             current_row = []
             for column in range(columns):
-                boton_celula = Button(self, text="", borderwidth=0, width=2, bg="White")
+                boton_celula = Button(self, text="", borderwidth=0, width=2, bg="White",
+                                      command=lambda row=row, col=column: self.set(row, col, "Red"))
                 boton_celula.grid(row=row, column=column, sticky="nsew", padx=1, pady=1)
                 current_row.append(boton_celula)
             self._widgets.append(current_row)
